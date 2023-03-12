fc3b97ee3ce045a605fb0b86f627326ec476ecba
alexander-29-09 <121211171+alexander-29-09@users.noreply.github.com>
2023-03-12 10:29:25 -0600

    Primer commit
    Creación de portafolio con información básica

---
+++ Portafolio/script.js
@@ -0,0 +1,38 @@
+
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
+
+
+window.onscroll = function() { efectoHabilidades() };
+
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300) {
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("C#").classList.add("barra-progreso3");
+        document.getElementById("DB").classList.add("barra-progreso4");
+    }
+
+}
